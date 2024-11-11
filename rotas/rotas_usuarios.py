8568f61e2407cb1a619cd5f8fd3cf15b28b81a8d
EnzoZKe <ebs.sarante@gmail.com>
2024-11-11 14:16:47 -0300

    vercel funciona

--- rotas/rotas_usuarios.py
+++ rotas/rotas_usuarios.py
@@ -40,7 +40,8 @@ def login_usuario():
         cursor.close()
         fechar_conexao(conn)
         
-        if senhaBanco and checar_senha(senhaBanco['SENHA'], SENHA):
+        # Verificar se senhaBanco existe e se contém a chave 'SENHA'
+        if senhaBanco and 'SENHA' in senhaBanco and checar_senha(senhaBanco['SENHA'], SENHA):
             session['usuario'] = {
                 'id_usuario': senhaBanco['ID_USUARIO'],
                 'email': senhaBanco['EMAIL'],
@@ -48,12 +49,12 @@ def login_usuario():
                 'pfp': senhaBanco['PFP'],
                 'usuario_tipo': senhaBanco['USUARIO_TIPO']
             }
-            
             return redirect(url_for('home'))
         else:
-            return render_template('login.html', mensagem = 'Login Incorreto')
+            return render_template('login.html', mensagem='Login Incorreto')
             
     return render_template('login.html')
+
         
 def checar_senha(senhaBanco, senha):
     senha_convertida = sha256(senha.encode('utf-8')).hexdigest()
